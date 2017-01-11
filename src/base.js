f512817a1f043b4bc1472dca63f2493c9ff6c6eb
kelly <kelly.n.waite@gmail.com>
2017-01-11 11:54:16 -0700

    Use re-base to add firebase; Link firebase with fishes state; Local storage track order

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBKDYb5uIXyy-E86nQ5sWa9ZSpUe3F_jBE",
+  authDomain: "catch-of-the-day-cb0e9.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-cb0e9.firebaseio.com",
+})
+
+export default base;
